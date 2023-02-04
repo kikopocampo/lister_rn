2139c0bc67c34de0f32f97abd1ea65faf22eb8d3
Francisco <kikopocampo@gmail.com>
2023-02-03 18:04:47 -0700

    feat: scrollview

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredText(enteredText);
+  }
+  function addGoalHandler() {
+    setTodo((prev) => [...prev, enteredText]);
+    setEnteredText("");
+  }
+  // console.log(todo);
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="What do you need to do?"
           style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredText}
         />
-        <Button title="Add to list" />
+        <Button title="Add to list" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>Your List</Text>
+        <ScrollView>
+          {todo.map((el, i) => {
+            return (
+              <View key={i} style={styles.goalItem}>
+                <Text style={styles.goalText}>{el}</Text>
+              </View>
+            );
+          })}
+        </ScrollView>
       </View>
     </View>
   );
@@ -41,5 +70,15 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "darkgreen",
+  },
+  goalText: {
+    textAlign: "center",
+    color: "white",
   },
 });
